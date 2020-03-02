25a7cdce790a67017b790d3139aa8d82991d7513
Jenn Nelson <jnelson@edenpr.org>
2020-03-02 07:36:30 -0600

    files for Monday

---
+++ monday/Songs.java
@@ -0,0 +1,150 @@
+package monday;
+
+public class Songs {
+	// the variables we are going to use.
+	
+	public static String helloSong = "Hello, it's me\n" + 
+			"I was wondering if after all these years you'd like to meet\n" + 
+			"To go over everything\n" + 
+			"They say that time's supposed to heal ya\n" + 
+			"But I ain't done much healing\n" + 
+			"\n" + 
+			"Hello, can you hear me?\n" + 
+			"I'm in California dreaming about who we used to be\n" + 
+			"When we were younger and free\n" + 
+			"I've forgotten how it felt before the world fell at our feet\n" + 
+			"\n" + 
+			"There's such a difference between us\n" + 
+			"And a million miles\n" + 
+			"\n" + 
+			"Hello from the other side\n" + 
+			"I must've called a thousand times\n" + 
+			"To tell you I'm sorry\n" + 
+			"For everything that I've done\n" + 
+			"But when I call you never\n" + 
+			"Seem to be home\n" + 
+			"\n" + 
+			"Hello from the outside\n" + 
+			"At least I can say that I've tried\n" + 
+			"To tell you I'm sorry\n" + 
+			"For breaking your heart\n" + 
+			"But it don't matter, it clearly\n" + 
+			"Doesn't tear you apart anymore\n" + 
+			"\n" + 
+			"Hello, how are you?\n" + 
+			"It's so typical of me to talk about myself, I'm sorry\n" + 
+			"I hope that you're well\n" + 
+			"Did you ever make it out of that town\n" + 
+			"Where nothing ever happened?\n" + 
+			"\n" + 
+			"It's no secret\n" + 
+			"That the both of us\n" + 
+			"Are running out of time\n" + 
+			"\n" + 
+			"So hello from the other side (other side)\n" + 
+			"I must've called a thousand times (thousand times)\n" + 
+			"To tell you I'm sorry\n" + 
+			"For everything that I've done\n" + 
+			"But when I call you never\n" + 
+			"Seem to be home\n" + 
+			"\n" + 
+			"Hello from the outside (outside)\n" + 
+			"At least I can say that I've tried (I've tried)\n" + 
+			"To tell you I'm sorry\n" + 
+			"For breaking your heart\n" + 
+			"But it don't matter, it clearly\n" + 
+			"Doesn't tear you apart anymore\n" + 
+			"\n" + 
+			"Oh, anymore\n" + 
+			"Oh, anymore\n" + 
+			"Oh, anymore\n" + 
+			"Anymore\n" + 
+			"\n" + 
+			"Hello from the other side (other side)\n" + 
+			"I must've called a thousand times (thousand times)\n" + 
+			"To tell you I'm sorry\n" + 
+			"For everything that I've done\n" + 
+			"But when I call you never\n" + 
+			"Seem to be home\n" + 
+			"\n" + 
+			"Hello from the outside (outside)\n" + 
+			"At least I can say that I've tried (I've tried)\n" + 
+			"To tell you I'm sorry\n" + 
+			"For breaking your heart\n" + 
+			"But it don't matter, it clearly\n" + 
+			"Doesn't tear you apart anymore";
+	
+	public static String burningDownTheHouseSong = "Watch out you might get what you're after\n" + 
+			"Boom babies strange but not a stranger\n" + 
+			"I'm an ordinary guy\n" + 
+			"Burning down the house\n" + 
+			"Hold tight wait till the party's over\n" + 
+			"Hold tight We're in for nasty weather\n" + 
+			"There has got to be a way\n" + 
+			"Burning down the house\n" + 
+			"Here's your ticket pack your bag\n" + 
+			"Time for jumpin' overboard\n" + 
+			"The transportation is here\n" + 
+			"Close enough but not too far\n" + 
+			"Maybe you know where you are\n" + 
+			"Fightin' fire with fire\n" + 
+			"All wet! Hey you might need a raincoat\n" + 
+			"Shakedown! Dreams walking in broad daylight\n" + 
+			"Three hun-dred six-ty five de-grees\n" + 
+			"Burning down the house\n" + 
+			"It was once upon a place sometimes I listen to myself\n" + 
+			"Gonna come in first place\n" + 
+			"People on their way to work and baby what did you except\n" + 
+			"Gonna burst into flame…";
+	
+	public static String westEndGirlsSong = "Sometimes you're better off dead\n" + 
+			"There's a gun in your hand it's pointing at your head\n" + 
+			"You think you're mad too unstable\n" + 
+			"Kicking in chairs and knocking down tables\n" + 
+			"In a restaurant in a West end town\n" + 
+			"Call the police there's a mad man around\n" + 
+			"Running down underground\n" + 
+			"To a dive bar in a West end town\n" + 
+			"In a West end town a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"In a West end town in a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"West end girls\n" + 
+			"Too many shadows whispering voices\n" + 
+			"Faces on posters too many choices\n" + 
+			"If when why what how much have you got\n" + 
+			"Have you got it do you get it\n" + 
+			"If so how often\n" + 
+			"Which do you choose\n" + 
+			"A hard or soft option\n" + 
+			"(How much do you need)\n" + 
+			"In a West end town a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"In a West end town in a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"West end girls\n" + 
+			"West end girls\n" + 
+			"In a West end town a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"Ooh a West end town in a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"West end girls\n" + 
+			"You got a heart of glass or a heart of stone\n" + 
+			"Just you wait till I get you home\n" + 
+			"We've got no future\n" + 
+			"We've got no past\n" + 
+			"Here today built to last\n" + 
+			"In every city and every nation\n" + 
+			"From Lake Geneva to the Finland station\n" + 
+			"(How far have you been)\n" + 
+			"In a West end town a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"In a West end town in a dead end world\n" + 
+			"The East end boys and West end girls\n" + 
+			"West end girls\n" + 
+			"West end girls\n" + 
+			"West end girls\n" + 
+			"(How far have you been)\n" + 
+			"Girls";
+
+}
